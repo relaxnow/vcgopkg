3f5082c38fc2337b585a8831857eb683957c8d0c
Boy Baukema <bbaukema@veracode.com>
2021-04-04 23:13:15 +0200

    Get Windows functional test to fail on difference

--- test/go-detailedreport-to-csv/test.ps1
+++ test/go-detailedreport-to-csv/test.ps1
@@ -1,5 +1,11 @@
+Remove-Item "$PSScriptRoot\temp" -Recurse
 Copy-Item -Path "$PSScriptRoot\input" -Destination "$PSScriptRoot\temp" -Recurse
 & "$PSScriptRoot\..\..\vcgopkg.exe" $PSScriptRoot\temp
 $fsInput = Get-ChildItem -Recurse -path $PSScriptRoot\temp
 $fsOutput = Get-ChildItem -Recurse -path $PSScriptRoot\output
-Compare-Object -ReferenceObject $fsInput -DifferenceObject $fsOutput
+$c = Compare-Object -ReferenceObject $fsInput -DifferenceObject $fsOutput
+Remove-Item "$PSScriptRoot\temp" -Recurse
+
+if ($c -ne 0) {
+    exit 1
+}
