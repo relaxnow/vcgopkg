0a1ad96f52f7c7b4138a3c20b51a5bef7681ee00
Boy Baukema <bbaukema@veracode.com>
2021-04-16 22:35:42 +0200

    Write output on test failure

--- test/go-detailedreport-to-csv/test.ps1
+++ test/go-detailedreport-to-csv/test.ps1
@@ -1,10 +1,11 @@
 Copy-Item -Path "$PSScriptRoot\input" -Destination "$PSScriptRoot\temp" -Recurse
 & "$PSScriptRoot\..\..\vcgopkg.exe" $PSScriptRoot\temp
-$fsInput = Get-ChildItem -Recurse -path $PSScriptRoot\temp
+$fsInput  = Get-ChildItem -Recurse -path $PSScriptRoot\temp
 $fsOutput = Get-ChildItem -Recurse -path $PSScriptRoot\output
 $c = Compare-Object -ReferenceObject $fsInput -DifferenceObject $fsOutput
 Remove-Item "$PSScriptRoot\temp" -Recurse
 
 if ($c -ne 0) {
+    Write-Output $c
     exit 1
 }
