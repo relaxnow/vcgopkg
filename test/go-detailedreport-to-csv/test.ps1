ee258010f8719c6823507db9a657dcd4484f6179
Boy Baukema <bbaukema@veracode.com>
2021-04-17 01:31:12 +0200

    More powershell debugging

--- test/go-detailedreport-to-csv/test.ps1
+++ test/go-detailedreport-to-csv/test.ps1
@@ -2,6 +2,12 @@ Remove-Item "$PSScriptRoot\temp" -Recurse -ErrorAction Ignore
 Copy-Item -Path "$PSScriptRoot\input" -Destination "$PSScriptRoot\temp" -Recurse
 & "$PSScriptRoot\..\..\vcgopkg.exe" "$PSScriptRoot\temp" "-20060102150405"
 
+if( $LASTEXITCODE -eq 0 ) {
+	Write-Output "vcgopkg exited successfully"
+} else {
+    Write-Output "vcgopkg failed"
+}
+
 Move-Item -Path $PSScriptRoot\temp -Destination $PSScriptRoot\test
 $fsTemp  = Get-ChildItem -Recurse -path $PSScriptRoot\test
 Move-Item -Path $PSScriptRoot\test -Destination $PSScriptRoot\temp
@@ -12,8 +18,16 @@ Move-Item -Path $PSScriptRoot\test -Destination $PSScriptRoot\output
 
 $c = Compare-Object -ReferenceObject $fsOutput -DifferenceObject $fsTemp
 
+if( $LASTEXITCODE -eq 0 ) {
+	Write-Output "Compare exited successfully"
+} else {
+    Write-Output "Compare failed"
+}
+
 if ($c -ne 0) {
+    Write-Output "Compare results:"
     Write-Output $c
     exit 1
+} else {
+    exit 0
 }
-exit 0
