0095e53e5d2586a7a1629754d2630027271d8234
Boy Baukema <bbaukema@veracode.com>
2021-04-17 00:24:26 +0200

    Make Windows compare against same exact path

--- test/go-detailedreport-to-csv/test.ps1
+++ test/go-detailedreport-to-csv/test.ps1
@@ -1,9 +1,16 @@
+Remove-Item "$PSScriptRoot\temp" -Recurse
 Copy-Item -Path "$PSScriptRoot\input" -Destination "$PSScriptRoot\temp" -Recurse
 & "$PSScriptRoot\..\..\vcgopkg.exe" $PSScriptRoot\temp 20060102150405
-$fsInput  = Get-ChildItem -Recurse -path $PSScriptRoot\temp
-$fsOutput = Get-ChildItem -Recurse -path $PSScriptRoot\output
-$c = Compare-Object -ReferenceObject $fsInput -DifferenceObject $fsOutput
-Remove-Item "$PSScriptRoot\temp" -Recurse
+
+Move-Item -Path $PSScriptRoot\temp -Destination $PSScriptRoot\test
+$fsTemp  = Get-ChildItem -Recurse -path $PSScriptRoot\test
+Move-Item -Path $PSScriptRoot\test -Destination $PSScriptRoot\temp
+
+Move-Item -Path $PSScriptRoot\output -Destination $PSScriptRoot\test
+$fsOutput = Get-ChildItem -Recurse -path $PSScriptRoot\test
+Move-Item -Path $PSScriptRoot\test -Destination $PSScriptRoot\output
+
+$c = Compare-Object -ReferenceObject $fsOutput -DifferenceObject $fsTemp
 
 if ($c -ne 0) {
     Write-Output $c
