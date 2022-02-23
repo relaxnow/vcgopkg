f35a3c058a7a880d6dcdaef3b6390b922f26e457
Boy Baukema <boy@relaxnow.nl>
2022-02-23 10:56:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#Date:22/02/2022
-#Author:Boy Baukema
-#Purpose:Install Winres and Veracode GO Packager
-
 go install github.com/tc-hib/go-winres@latest
 go-winres make
 GOOS=windows GOARCH=amd64 go build -o vcgopkg-amd64.exe
-
-#END
