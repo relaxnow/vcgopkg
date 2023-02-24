27a65fc613ea5dd5347aaa0878739bc3cb11d4d8
Boy Baukema <>
2023-02-24 16:34:38 +0100

    Fix functional tests on Linux

--- test/go-detailedreport-to-csv/test.sh
+++ test/go-detailedreport-to-csv/test.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 rm -rf "$DIR/temp"
 cp -Rf "$DIR/input" "$DIR/temp"
 $DIR/../../vcgopkg "$DIR/temp" "-20060102150405"
-unzip -l "$DIR/temp/go-detailedreport-to-csv/veracode/go-detailedreport-to-csv_cmd--go-detailedreport-to-csv-20060102150405.zip" | awk '{ print $4 }' | sort > $DIR/temp/temp.zip.log
-unzip -l "$DIR/output/go-detailedreport-to-csv/veracode/go-detailedreport-to-csv_cmd--go-detailedreport-to-csv-20060102150405.zip" | awk '{ print $4 }' | sort > $DIR/temp/output.zip.log
+unzip -l "$DIR/temp/go-detailedreport-to-csv/veracode/go-detailedreport-to-csv_cmd--go-detailedreport-to-csv--main-20060102150405.zip" | awk '{ print $4 }' | sort > $DIR/temp/temp.zip.log
+unzip -l "$DIR/output/go-detailedreport-to-csv/veracode/go-detailedreport-to-csv_cmd--go-detailedreport-to-csv--main-20060102150405.zip" | awk '{ print $4 }' | sort > $DIR/temp/output.zip.log
 diff $DIR/temp/output.zip.log $DIR/temp/temp.zip.log
 rm -rf "$DIR/temp"
